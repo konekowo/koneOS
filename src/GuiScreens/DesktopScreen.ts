77f1d238d22c7a0c46090acda73c25887afc05c0
kone <70107937+konekowo@users.noreply.github.com>
2023-12-02 12:51:41 -0500

    Update DesktopScreen.ts

--- src/GuiScreens/DesktopScreen.ts
+++ src/GuiScreens/DesktopScreen.ts
@@ -8,7 +8,7 @@ export class DesktopScreen{
     public constructor(bgImgWidth:number, bgImgHeight:number, bgSprite: PIXI.Sprite, app:PIXI.Application) {
         console.log("Cleaned Screens and loops! (which probably, hopefully cleaned out some of the memory)")
         app.stage.addChild(bgSprite);
-        const testApp = new FileExplorer(app);
+        const testApp = new FileExplorer();
         const taskBar = new TaskBar(app);
         const menuBar = new MenuBar(app);
     }
